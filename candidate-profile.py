2661f1ae372c02da39ab511c1a6708ff6d29a422
Henry Owade <oyotohenry2021@gmail.com>
2026-06-11 20:01:17 +0300

    Day1: Data Types Practice (candidate profile)

---
+++ candidate-profile.py
@@ -0,0 +1,23 @@
+#1. Data Types and Variables in Python
+
+roll_number = 10
+height = 5.7
+name = "Alice"
+is_student = True
+age = 20
+price = 99.99
+country = "Kenya"
+is_employed = False
+x = 10
+y = "10"
+
+print(type(roll_number))
+print(type(height))
+print(type(name))
+print(type(is_student))
+print(type(age))
+print(type(price))
+print(type(country))
+print(type(is_employed))
+print(type(x))
+print(type(y))
