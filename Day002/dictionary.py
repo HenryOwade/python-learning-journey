73d215ba110bc5aaddf1d2faf10dd0d3facd6dfc
Henry Owade <oyotohenry2021@gmail.com>
2026-06-12 17:17:02 +0300

    Made Some changes to the dictionary

---
+++ Day002/dictionary.py
@@ -0,0 +1,25 @@
+# Dictionary in Python
+
+student = {
+    "name": "Alice",
+    "age": 20,
+    "course": "Python"
+}
+print(student)
+
+print(type(student))
+
+student = {
+    "name": "Alice",
+    "age": 20,
+    "course": "Data Science"
+}
+print(student)
+
+student["city"] = "New York"
+
+print(student)
+
+del student["city"]
+
+print(student)
