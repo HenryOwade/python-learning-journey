9c4120f671415b563f15c7835980183c8d3eb329
Henry Owade <oyotohenry2021@gmail.com>
2026-06-14 16:04:36 +0300

    Day 3: Created operators.py

---
+++ Day003/operators.py
@@ -0,0 +1,13 @@
+# Day 003
+
+## Operators
+
+a = 10
+b = 2
+
+print("Addition of the Two Numbers is:", a+b)
+print("Subtraction of the Two Numbers is:", a-b)
+print("Multiplication of the Two Numbers is:", a*b)
+print("Division of the Two Numbers is:", a/b)
+print("Floor Division of the Two Numbers is:", a//b)
+print("Exponential of the Two Numbers is:", a**b)
