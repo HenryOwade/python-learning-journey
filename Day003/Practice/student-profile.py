963b3f7e7e02f8da0cc209e71bdf9b379e61e5bf
Henry Owade <oyotohenry2021@gmail.com>
2026-06-14 16:54:36 +0300

    Weekend exercises; variables, strings and data structures

---
+++ Day003/Practice/student-profile.py
@@ -0,0 +1,23 @@
+# Practice exercises Day 3
+
+name = "Henry"
+age = 24
+country = "Kenya"
+
+# Creating a Dictionary
+
+student = {
+"name":"Henry", 
+"age":24, 
+"country":"Kenya"
+}
+
+student["course"] = "Python"
+
+student["city"] = "Nairobi"
+
+del student["city"]
+
+print(student)
+
+print(type(student))
