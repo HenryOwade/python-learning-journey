8550704a9e5c80c21f6ef194d2275f2c0ef53e38
Henry Owade <oyotohenry2021@gmail.com>
2026-06-14 15:20:47 +0300

    Day 3: Created Day003 folder

---
+++ Day003/strings.py
@@ -0,0 +1,18 @@
+# Day003
+
+## Strings
+
+first_name = "john"
+second_name = "conner"
+
+Full_name = first_name + " " + second_name
+
+print(Full_name)
+
+
+
+text = "Python Programming"
+
+length = len(text)
+
+print("The length of the given string is :", length)
