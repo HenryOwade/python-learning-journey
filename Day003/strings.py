f0b5de172a753ebba7ad3fc65a057ec3fba740f1
Henry Owade <oyotohenry2021@gmail.com>
2026-06-14 15:54:06 +0300

    Day 3: Learned Strings in Python, strings matching, string slicing, replacing and checking if a specific part of a string is present

--- Day003/strings.py
+++ Day003/strings.py
@@ -15,4 +15,52 @@ text = "Python Programming"
 
 length = len(text)
 
+# Checking and printing the length
+
 print("The length of the given string is :", length)
+
+# Converting to Uppercase and Lowercase
+
+print("Text in Uppercase:", text.upper())
+print("Text in lowercase:", text.lower())
+
+# String Slicing
+
+text = "Learn Python with Simplilearn"
+
+print("The first 12 characters are:", text[:12])
+print("The rest of the characters after 12 char are:", text[12:])
+
+# Text Matching and Replacing
+
+text = "I like programming in Java"
+next_text = text.replace("Java", "Python")
+
+print(next_text)
+
+# Check if a specific part of a string is present
+
+text = "I like Programing in Python"
+
+if "Python" in text:
+    print("Text is Found")
+
+else:
+    print("Text is Not Found")
+
+text = "I like programming in Java"
+
+if "Python" in text:
+    print("Text is Found")
+
+else:
+    print("Text is NOT Found")
+    
+
+
+    
+
+
+
+
+
