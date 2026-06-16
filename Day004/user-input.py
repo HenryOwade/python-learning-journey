12d17c2718a28ffde338e8c8a13c9ab9b241a91f
Henry Owade <oyotohenry2021@gmail.com>
2026-06-16 11:49:55 +0300

    Day 4: Created Day004 folder and created user-input.py

---
+++ Day004/user-input.py
@@ -0,0 +1,13 @@
+# Day 004
+
+## User Inputs
+
+num_1 = float(input("Enter the First Number: "))
+num_2 = float(input("Enter the Second Number: "))
+
+result = num_1 + num_2
+
+print("Addition of the two Numbers is:", result)
+
+name = input("Enter your Name")
+print(name)
