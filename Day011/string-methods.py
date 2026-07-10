6bea03b1dac981196bd0d4d04a184b80487b64bf
Henry Owade <oyotohenry2021@gmail.com>
2026-07-10 20:59:04 +0300

    Learned the methods used in strings

--- Day011/string-methods.py
+++ Day011/string-methods.py
@@ -1,1 +1,9 @@
 course = "Python for Beginners"
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find("P")) # Returns the index of the character P
+print(course.find("o")) # Returns the index of the character o
+print(course.replace("Beginners", "Absolute Beginners"))
+print("Python" in course) # Output a boolean value True
+print("python" in course) # Output a boolean value False
