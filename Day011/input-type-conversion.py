551d7e115ef8b210a7364c9fcb999613ce2eb876
Henry Owade <oyotohenry2021@gmail.com>
2026-07-10 15:49:07 +0300

    Day011 Created input-type-conversion.py and learned about conversion of user inputs from string to float to integers and vise versa

---
+++ Day011/input-type-conversion.py
@@ -0,0 +1,9 @@
+birth_year = input("Enter your Birth Year ")
+print(type(birth_year))
+age = 2026 - int(birth_year)
+print(type(age))
+print(age)
+
+weight_pounds = input("Enter your Weight in Pounds ") # pounds = lbs
+weight_kgs = 0.453592 * int(weight_pounds)
+print(weight_kgs)
