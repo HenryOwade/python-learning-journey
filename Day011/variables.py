923d9382068de4892aa93a0b436cc1631d3adfec
Henry Owade <oyotohenry2021@gmail.com>
2026-07-10 15:07:51 +0300

    Day011 Created input.py

--- Day011/variables.py
+++ Day011/variables.py
@@ -1,1 +1,15 @@
-class
+price = 10
+price = 20  #integer
+rating = 4.9 #float
+name = "Henry" #String
+is_published = True #Boolean
+print(price)
+print(rating)
+
+# We check in a patient name called Henry Owade
+# He is 20 years old and he is a new patient
+
+full_name = "Henry Owade"
+age = 20
+is_new = True
+print(full_name)
