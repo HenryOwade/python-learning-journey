7282b0198c4c8946c49b20ab786db2e04db87b61
Henry Owade <oyotohenry2021@gmail.com>
2026-07-10 21:59:04 +0300

    Learned about Operators

---
+++ Day011/operators.py
@@ -0,0 +1,24 @@
+print(10 + 3)
+print(10 - 3)
+print(10 / 3)
+print(10 % 3)
+print(10 * 3)
+print(10 // 3)
+print(10 ** 3)
+
+x = 10
+x = x + 3
+print(x)
+
+# Augmented Assignment Operator
+x = 20
+x += 10
+print(x)
+
+y = 30
+y -= 20
+print(y)
+
+z = 40
+z *= 2
+print(z)
