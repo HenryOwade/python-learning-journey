b3c63c4cf48f4ef3dcde32cb2f967fdaea2f8b59
Henry Owade <oyotohenry2021@gmail.com>
2026-07-10 16:14:20 +0300

    Day011 Learned more about strings and use of square brackets when printing texts

---
+++ Day011/strings.py
@@ -0,0 +1,28 @@
+course = "Python's course for Beginners" # Use bouble quotes for strings with apostrophe
+print(course)
+
+# To define a string with Multiple Lines, use tripple quotes i. single or double quotes.
+course = """ 
+Hi Henry,
+
+Here is our first email to you.
+
+Thank You.
+Support Team.
+"""
+
+print(course)
+
+course = "Python for Beginners"
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:5])
+
+another = course[:]
+print(another) # another is just a copy of course
+
+name = "Henry"
+print(name[1:-1])
+
