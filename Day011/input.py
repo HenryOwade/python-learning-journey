2c7827f5bae9beaecacc80e99a4f96b0af28a240
Henry Owade <oyotohenry2021@gmail.com>
2026-07-10 15:29:58 +0300

    Learned inputs

---
+++ Day011/input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+print("Hi " + name) # same as print("Hi", name) the comma(,) automatically creates space when printing. 
+
+name = input("What is your name? ")
+colour = input("What is your favourite colour? ")
+print(name + " likes " + colour) # same as print(name, "likes", colour)
