d3214a7a879afcf78e27960382f8135b0939699e
Henry Owade <oyotohenry2021@gmail.com>
2026-07-10 16:32:40 +0300

    Learned about formatted Strings

---
+++ Day011/formatted-strings.py
@@ -0,0 +1,7 @@
+first_name = "Henry"
+last_name = "Owade"
+message = first_name + " [" + last_name + "] is a coder" 
+print(message)
+# To simplify this, use formatted strings i.e starting with (f)
+message = f"{first_name} [{last_name}] is a coder"
+print(message)
