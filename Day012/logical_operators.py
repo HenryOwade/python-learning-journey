d234707251aad5f917bef9bbad8689e746ad2e6a
Henry Owade <oyotohenry2021@gmail.com>
2026-07-11 12:51:33 +0300

    Learned about logical operators and, or, and not

---
+++ Day012/logical_operators.py
@@ -0,0 +1,8 @@
+has_high_income = True
+has_good_credit = True
+has_criminal_record = True
+
+if has_high_income and has_good_credit and not has_criminal_record:
+    print("Eligible for Loan")
+else:
+    print("Not Eligible for loan")
