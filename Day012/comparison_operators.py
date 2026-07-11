50aeaeba6de688992b57260a041e85e9ca54d8e8
Henry Owade <oyotohenry2021@gmail.com>
2026-07-11 13:26:54 +0300

    Learned about comparison operators

---
+++ Day012/comparison_operators.py
@@ -0,0 +1,15 @@
+temperature = 30
+
+if temperature >= 30:
+    print("It is a hot day")
+else:
+    print("It is not a hot day")
+
+name = input("Enter your name: ")
+
+if len(name) < 3:
+    print("Name must be at least 3 characters long")
+elif len(name) > 50:
+    print("Name can be a maximum of 50 characters")
+else:
+    print("Name looks good")
