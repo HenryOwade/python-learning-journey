38a588d417001a6456109e62aa1fe7c346596d3b
Henry Owade <oyotohenry2021@gmail.com>
2026-07-14 13:09:18 +0300

    Learned functions

---
+++ Day015/functions.py
@@ -0,0 +1,19 @@
+def greet_user():
+    print("Hi there!")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user()
+print("Finish")
+
+def come_to_class():
+    print(""" 
+You people in this class
+once you re through with your 
+home works, make sure you return your 
+plates and come back to class""")
+
+print("Today is Thursday")
+come_to_class()
+print("Be quick")
