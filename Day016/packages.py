08d79e4ab7dc772acde904a0a952fb1beb29a779
Henry Owade <oyotohenry2021@gmail.com>
2026-07-15 16:10:10 +0300

    Learned Packages

--- Day016/packages.py
+++ Day016/packages.py
@@ -3,4 +3,14 @@
 
 # Packages help keep large projects organized by seperating related code into 
 # different modules and folders.
- 
+
+# 1. First Way
+
+from ecommerce import shipping
+shipping.calc_shipping()
+
+# 2. Second Way
+
+from ecommerce.shipping import calc_shipping
+
+
