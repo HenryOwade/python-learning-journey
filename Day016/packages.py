1f2b6bb1988d91d5f1401d1ff9a887ee5c5bf453
Henry Owade <oyotohenry2021@gmail.com>
2026-07-15 16:02:45 +0300

    Created a new package called ecommerce

---
+++ Day016/packages.py
@@ -0,0 +1,6 @@
+# A package is a folder used to organize related Python modules (Python files)
+# into a structured folder
+
+# Packages help keep large projects organized by seperating related code into 
+# different modules and folders.
+ 
