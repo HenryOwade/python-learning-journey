0776ddb2f1b1075db77c2681864797d609bae49b
Henry Owade <oyotohenry2021@gmail.com>
2026-07-16 13:48:43 +0300

    Learned about PyPI and pip and installed openpyxl package

---
+++ Day017/pypi_and_pip.py
@@ -0,0 +1,6 @@
+# PyPI: This is an online repository where Python developers publish packages that other people can download and use.
+# pip: This is a tool used to install Python packages.
+
+# For example
+# pip install pandas
+# pip install openpyxl
