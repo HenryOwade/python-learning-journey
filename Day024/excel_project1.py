3c51b377c8953ede7880f11b5da33b43688f8f81
Henry Owade <oyotohenry2021@gmail.com>
2026-07-26 14:55:45 +0300

    Continuation of Day 24: Finished project on reading excel files

--- Day024/excel_project1.py
+++ Day024/excel_project1.py
@@ -1,21 +1,74 @@
+# This project was all about reading Excel File
+
+# from openpyxl import load_workbook
+
+# workbook = load_workbook("practice_file_1.xlsx")
+
+# # print(workbook)
+# # print(type(workbook))
+# # print(workbook.sheetnames)
+# # print(type(workbook.sheetnames))
+
+# sheet = workbook["Sales"]
+
+# print(sheet)
+# print(type(sheet))
+
+# print(sheet["A1"])
+# print(type(sheet["A1"]))
+
+# print(sheet["A1"].value)
+
+# print(sheet.cell(row=1, column=1))
+# print(sheet.cell(row=1, column=1).value)
+
+# for row in range(2, 7):
+
+#     row_data = []
+
+#     for column in range(1, 6):
+#         row_data.append(sheet.cell(row=row, column=column).value)
+
+#     print(row_data)
+
 from openpyxl import load_workbook
 
-workbook = load_workbook("practice_file_1.xlsx")
+files = [
+     "practice_file_1.xlsx",
+     "practice_file_2.xlsx",
+     "practice_file_3.xlsx",
+     "practice_file_4.xlsx",
+     "practice_file_5.xlsx"
+     ]
 
-print(workbook)
-print(type(workbook))
-print(workbook.sheetnames)
-print(type(workbook.sheetnames))
+all_data = []
 
-sheet = workbook["Sales"]
+for filename in files:
 
-print(sheet)
-print(type(sheet))
+    workbook = load_workbook(filename)
+    sheet = workbook["Sales"]
+#     print(sheet.title)
+#     print(workbook)
+#     cell = sheet["A1"]
 
-print(sheet["A1"])
-print(type(sheet["A1"]))
+#     print(cell)
+#     print(cell.value)
+#     print(cell.row)
+#     print(cell.column)
+#     print(cell.coordinate)
 
-print(sheet["A1"].value)
+    for row in range(2, 7):
 
-print(sheet.cell(row=1, column=1))
-print(sheet.cell(row=1, column=1).value)
+        row_data = []
+
+        for column in range(1, 6):
+            row_data.append(sheet.cell(row=row, column=column).value)
+
+        all_data.append(row_data)
+
+print(type(all_data))
+print(len(all_data))
+print()
+
+for row in all_data:
+    print(row)
